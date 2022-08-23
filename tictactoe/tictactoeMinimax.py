bb58e93b3826262061fbb93d60465b93a6460583
Sau Juliana <julienyange@gmail.com>
2022-08-23 12:53:20 +0300

    add minimax func to create unbeatable computer bot

--- tictactoe/tictactoeMinimax.py
+++ tictactoe/tictactoeMinimax.py
@@ -17,18 +17,18 @@ def printBoard(board):
     print("\n")
 
 
-def spaceisFree(position):
+def spaceIsFree(position):
     if board[position] == ' ':
         return True
     return False
 
 
 def insertLetter(letter, position):
-    if spaceisFree(position):
+    if spaceIsFree(position):
         board[position] = letter
         printBoard(board)
         if checkDraw():
-            print("Draw")
+            print("Draw!")
             exit()
         if checkWin():
             if letter == 'X':
@@ -40,18 +40,9 @@ def insertLetter(letter, position):
         return
     else:
         print("Invalid position!")
-        while True:
-            try:
-                position = int(input("Please enter a new position: "))
-                if 1 <= position <= 9:
-                    insertLetter(player, position)
-                    break
-                elif position not in range(1, 11) or position == '':
-                    print("Enter a valid number!")
-                else:
-                    print("Enter a number bewteen 1 and 9")
-            except ValueError:
-                print("Enter a number!")
+        position = int(input("Please enter a new position: "))
+        insertLetter(letter, position)
+        return
 
 
 def checkWin():
@@ -78,6 +69,30 @@ def checkWin():
         return False
 
 
+def checkWhichMarkWon(mark):
+    # check horizontal
+    if (board[1] == board[2] and board[1] == board[3] and board[1] == mark):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
+        return True
+    # check vertical
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
+        return True
+    # check diagonal
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
+        return True
+    else:
+        return False
+
+
 def checkDraw():
     for key in board.keys():
         if board[key] == ' ':
@@ -86,40 +101,55 @@ def checkDraw():
 
 
 def playerMove():
-    while True:
-        try:
-            position = int(input("Enter position for 'O': "))
-            if 1 <= position <= 9:
-                insertLetter(player, position)
-                break
-            elif position not in range(1, 11) or position == '':
-                print("Enter a valid number!")
-            else:
-                print("Enter a number bewteen 1 and 9")
-        except ValueError:
-            print("Enter a number!")
-
+    position = int(input("Enter a position for 'O': "))
+    insertLetter(player, position)
     return
 
 
 def computerMove():
-    while True:
-        try:
-            position = int(input("Enter position for 'X': "))
-            if 1 <= position <= 9:
-                insertLetter(computer, position)
-                break
-            elif position not in range(1, 11) or position == '':
-                print("Enter a valid number!")
-            else:
-                print("Enter a number bewteen 1 and 9")
-        except ValueError:
-            print("Enter a number!")
+    bestScore = -800
+    bestMove = 0
+    for key in board.keys():
+        if board[key] == ' ':
+            board[key] = computer
+            score = minimax(board, False)
+            if score > bestScore:
+                bestScore = score
+                bestMove = key
+    insertLetter(computer, bestMove)
     return
+
+
+def minimax(board, isMaximizing):
+    if checkWhichMarkWon(computer):
+        return 1
+    elif checkWhichMarkWon(player):
+        return -1
+    elif checkDraw():
+        return 0
+
+    if isMaximizing:
+        bestScore = -800
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = computer
+                score = minimax(board, False)
+                board[key] = ' '
+                if score > bestScore:
+                    bestScore = score
+        return bestScore
+    else:
+        bestScore = 800
+        for key in board.keys():
+            if board[key] == ' ':
+                board[key] = player
+                score = minimax(board, True)
+                board[key] = ' '
+                if score < bestScore:
+                    bestScore = score
+        return bestScore
 
 
 while not checkWin():
     computerMove()
     playerMove()
-
-printBoard(board)
