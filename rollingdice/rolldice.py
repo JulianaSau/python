8362886de2ece530f929b385b415106b0b3c15b8
Sau Juliana <julienyange@gmail.com>
2022-10-01 09:55:26 +0300

    🎉 rolling dice done

---
+++ rollingdice/rolldice.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+# ToDo:
+# import random
+import random
+
+# set max and min values and intervals
+count = 0
+
+sum1 = 0
+sum2 = 0
+sum3 = 0
+sum4 = 0
+sum5 = 0
+sum6 = 0
+
+
+def getRandomNumber(num):
+    global sum1, sum2, sum3, sum4, sum5, sum6
+    if num >= 0/6 and num < 1/6:
+        sum1 += 1
+    elif num >= 1/6 and num < 2/6:
+        sum2 += 1
+    elif num >= 2/6 and num < 3/6:
+        sum3 += 1
+    elif num >= 3/6 and num < 4/6:
+        sum4 += 1
+    elif num >= 4/6 and num < 5/6:
+        sum5 += 1
+    elif num >= 5/6 and num < 6/6:
+        sum6 += 1
+
+
+# while count < 1000 program runs
+while count < 1000:
+    result = random.random()
+    getRandomNumber(result)
+    count += 1
+
+
+# print table of values
+print("|  side   |   frequency   |    percentage   |")
+print("|---------|---------------|-----------------|")
+array = [sum1, sum2, sum3, sum4, sum5, sum6]
+totalsum = sum1 + sum2 + sum3 + sum4 + sum5 + sum6
+totalpercentage = 0
+for i in range(1, 7):
+    percentage = round(((array[i-1]/totalsum) * 100), 1)
+    totalpercentage += percentage
+    print(f"|    {i}    |      {array[i-1]}      |      {percentage}%      |")
+print("|---------|---------------|-----------------|")
+print(
+    f"|  total  |     {totalsum}      |      {round(totalpercentage)}%       |")
